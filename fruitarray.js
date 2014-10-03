40f358fbe88d66709aee2ec735d63a4333cd5c34
creatrixtiara <me@creatrixtiara.com>
2014-10-03 14:16:09 -0700

    Create fruitarray.js

---
+++ fruitarray.js
@@ -0,0 +1,5 @@
+// this was working a second ago and now jsbin just keeps throwing "unexpected token ;" errors at me
+var blahArray = ['apple', 'banana', 'carrot', 'durian', 'eggplant'];
+for (i = 0; i < blahArray.length; i++) {
+  console.log("On sale today " + blahArray[i]);
+}
